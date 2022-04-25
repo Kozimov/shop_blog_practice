58ee123ff9344f556fd43d63c576880e49f5c37b
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-25 11:34:46 +0500

    images cansee

--- shopblog/settings.py
+++ shopblog/settings.py
@@ -111,11 +111,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
-# STATICFILES_DIRS = ( os.path.join('static'), )
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- shopblog/urls.py
+++ shopblog/urls.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from backend.views import HomeView
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(),),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
