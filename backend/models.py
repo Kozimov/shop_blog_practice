c4365f6119db50d487319753bac67e547baa6951
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-26 12:57:51 +0500

    need to home html

--- backend/models.py
+++ backend/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
     size_three = models.CharField(max_length=20, null=True, blank=True)
     size_four = models.CharField(max_length=20, null=True, blank=True)
     brand = models.CharField(max_length=20, null=True, blank=True)
-    description = models.CharField(max_length=200, null=True, blank=True)
+    description = models.CharField(max_length=500, null=True, blank=True)
     color = models.CharField(max_length=100, null=True, blank=True)
     price = models.IntegerField(default=0)
     image = models.ImageField(upload_to="rasmlar/", null=True, blank=True)
