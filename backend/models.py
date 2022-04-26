aa43c4a85a2fe1ee4028aa0f7759e3b262c20031
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-26 10:27:54 +0500

    post models create

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+
+    name = models.CharField(max_length=20, null=True, blank=True)
+    size = models.CharField(max_length=20, null=True, blank=True)
+    price = models.IntegerField(default=0)
+    image = models.ImageField(upload_to="rasmlar/", null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
--- shopblog/urls.py
+++ shopblog/urls.py
@@ -1,4 +1,3 @@
-from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
 
