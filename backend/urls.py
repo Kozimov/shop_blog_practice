d862072a288d727551c853d6d4512c722334cc96
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-26 10:47:51 +0500

    postviews create

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from backend.views import AboutView, ContactView, HomeView, ShopView
+from backend.views import AboutView, ContactView, HomeView, PostListView
 
 
 urlpatterns = [
     path('', HomeView.as_view()),
     path('about/', AboutView.as_view(), name="about"),
-    path('shop/', ShopView.as_view(), name="shop"),
     path('contact/', ContactView.as_view(), name="contact"),
+    path('shop/', PostListView.as_view(), name="shop"),
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render
 from django.views.generic import *
-
+from .models import *
 
 class HomeView(TemplateView):
     template_name = "home.html"
 
 class AboutView(TemplateView):
     template_name = "about.html"
-
-class ShopView(TemplateView):
-    template_name = "shop.html"
-
+    
 class ContactView(TemplateView):
     template_name = "contact.html"
+
+class PostListView(ListView):
+    template_name = "shop.html"
+    queryset = Post.objects.all()
+    context_object_name = "posts"
