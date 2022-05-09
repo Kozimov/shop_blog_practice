899e0b0b3d86774363b96e167c902bca21265b5f
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-05-09 17:15:14 +0500

    categorymonth admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post, Carusel
+from .models import CategoryMonth, Post, Carusel
 
 admin.site.register(Post)
 admin.site.register(Carusel)
+admin.site.register(CategoryMonth)
