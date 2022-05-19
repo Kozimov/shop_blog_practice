e38b10bfc14bd0843719af15509eda0f79bc03f0
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-05-19 19:46:14 +0500

    login shop

--- shopblog/urls.py
+++ shopblog/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
+
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('backend.urls')),
+    path('login/', LoginView.as_view(), name="login")
 ]
 
 
