b2edeeca9fddc0ce2ba26b3359bf34e171dbc66f
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-05-19 19:49:28 +0500

    settings url

--- shopblog/settings.py
+++ shopblog/settings.py
@@ -122,3 +122,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = "/"
